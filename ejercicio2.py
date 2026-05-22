e793a62b6f703086d40358ad2365dea97c99520e
juanecheverria2008xd <juanecheverria2008@gmail.com>
2026-05-21 22:31:23 -0500

    ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,4 @@
-## Escriba el nombre del alumno aquí: 
+## Escriba el nombre del alumno aquí: Juan Andres Echeverria Aguiar
 ##
-## Escriba su código a partir de la siguiente línea:
+## Escriba su código a partir de la siguiente línea: https://colab.research.google.com/drive/1HyDFPRIWRWZO38g-7QQJ3m6qI2zRfila?usp=sharing
 
